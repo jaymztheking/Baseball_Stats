337bd3d9510a16ce295b38c1cb5042bd090fe06b
jaymztheking <james.medaugh@gmail.com>
2016-04-01 21:42:34 -0600

    Add Multi Year scrape logic to testRun

--- testRun.py
+++ testRun.py
@@ -4,13 +4,16 @@ from PlayResultStats import ProcessPlayLog
 
 pw = 'h4xorz' #raw_input('Password? ')
 con = psycopg2.connect("dbname=bbstats user=bbadmin host=192.168.1.111 password=%s" % pw)
-
+year = 2015
 #Real Deal
-
-for file in enumerate(os.listdir('.\\Play by Play Logs\\')):
-    if '.EV' in file[1]:
-        filename = '.\\Play by Play Logs\\'+file[1]
-        a, b, c, d = ProcessPlayLog(filename, con)
+while year > 1992:
+    print year
+    for file in enumerate(os.listdir('.\\Play by Play Logs\\%s' % year)):
+        print(file)   
+        if '.EV' in file[1]:
+            filename = '.\\Play by Play Logs\\%s\\' % year +file[1]
+            a, b, c, d = ProcessPlayLog(filename, con)
+    year -= 1
 
 
 #Debug
