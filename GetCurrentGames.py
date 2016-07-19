3e086cc056bcb5aaf524f989abfb3d4221ac621d
James Medaugh <james.medaugh@cccs.edu>
2016-07-19 10:38:33 -0600

    Change Date Logic To Range

--- GetCurrentGames.py
+++ GetCurrentGames.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib2, datetime
 from BRParser import GamesParser
 
 savePath = '.\\Game HTML Saves\\'
@@ -10,17 +10,31 @@ day = str(input('What day?'))
 url = 'http://www.baseball-reference.com/games/standings.cgi?month=%s&day=%s&year=%s' % (month.zfill(2), day, year)
 '''
 
-html = urllib2.urlopen(url).read().decode('utf-8').replace('&#183;','*')
-listOfGames = []
-b= GamesParser()
-b.feed(html)
-for game in b.games:
-    gameNum = 1
-    if game[1]+game[0] in listOfGames:
-        gameNum += 1
-    fileName = savePath + '%s_@_%s_%s_%s' % (game[1], game[0], gameNum, year+month.zfill(2)+day.zfill(2))+'.html'
-    listOfGames.append(game[1]+game[0])
-    gameHTML = urllib2.urlopen(game[2]).read().decode('utf-8')
-    newFile = open(fileName, 'w')
-    newFile.write(gameHTML)
-    newFile.close()
+startDate = datetime.date(2016,5,7)
+endDate = datetime.date(2016,5,8) #datetime.date.today()
+oneDay = datetime.timedelta(1)
+
+while startDate < endDate:
+
+    year = str(startDate.year)
+    month = str(startDate.month)
+    day = str(startDate.day)
+
+    url = 'http://www.baseball-reference.com/games/standings.cgi?month=%s&day=%s&year=%s' % (month.zfill(2), day, year)
+
+    html = urllib2.urlopen(url).read().decode('utf-8').replace('&#183;','*')
+    listOfGames = []
+    b= GamesParser()
+    b.feed(html)
+    for game in b.games:
+        gameNum = 1
+        if game[1]+game[0] in listOfGames:
+            gameNum += 1
+        fileName = savePath + '%s_@_%s_%s_%s' % (game[1], game[0], gameNum, year+month.zfill(2)+day.zfill(2))+'.html'
+        listOfGames.append(game[1]+game[0])
+        gameHTML = urllib2.urlopen(game[2]).read().decode('utf-8')
+        newFile = open(fileName, 'w')
+        newFile.write(gameHTML)
+        newFile.close()
+
+    startDate = startDate + oneDay
