2d4154b0e64f76a434e65eb3174b48bb205b13aa
jaymztheking <james.medaugh@gmail.com>
2016-08-22 21:18:05 -0600

    Add troubleshooting queries and remove junk ones

---
+++ SQL Queries/LineupMatch.sql
@@ -0,0 +1,13 @@
+﻿select 
+h."NAME",
+l."PLAYER_POS",
+l."AT_BATS",
+l."RUNS",
+1."HITS",
+l."RBI",
+l."BB",
+'X' as "K",
+l."PA"
+from "LINEUP" l
+inner join "HITTER_STATS" h on l."PLAYER_KEY" = h."PLAYER_KEY"
+order by "TEAM_KEY", "PLAYER_BAT_NUM"
